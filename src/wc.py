cd801ec9727818bbb94a9246b9a804bc26edb6f7
manaal <phenomanaal@gmail.com>
2024-09-25 19:28:02 -0400

    allow input from stdin

--- src/wc.py
+++ src/wc.py
@@ -1,36 +1,79 @@
 #!/usr/bin/env python3
 from pathlib import Path
+import sys
 import argparse
 
-def byte_count(filepath: str | Path) -> int:   
+def byte_count(filepath: str | Path = None, content: str = None) -> int:   
     
-    return Path(filepath).stat().st_size
+    if filepath:
+        return Path(filepath).stat().st_size
+    else:
+        return len(content.encode('utf-8'))
 
-def line_count(filepath: str | Path) -> int:
+def line_count(filepath: str | Path = None, content: str = None) -> int:
 
-    filepath = Path(filepath).resolve()
+    if filepath:
+        filepath = Path(filepath).resolve()
+        
+        with open(filepath, 'r', encoding='utf-8') as f:
+            return sum(1 for line in f)
+    else:
+        return len(content.splitlines())
     
-    with open(filepath, 'r') as f:
-        return sum(1 for line in f)
-    
-def word_count(filepath: str | Path) -> int:
-    filepath = Path(filepath).resolve()
+def word_count(filepath: str | Path = None, content: str = None) -> int:
 
-    with open(filepath, 'r') as f:
-        text_file = f.read()
+    if filepath:
+        filepath = Path(filepath).resolve()
 
-    return len(text_file.split())
+        with open(filepath, 'r', encoding='utf-8') as f:
+            text_file = f.read()
+
+        return len(text_file.split())
+    else:
+        return len(content.split())
+
+def character_count(filepath: str | Path = None, content: str = None) -> int:
+    if filepath:
+        filepath = Path(filepath).resolve()
+
+        with open(filepath, 'r', encoding='utf-8') as f:
+            text_file = f.read()
+        text_file = text_file.replace('\r\n', '\n')
+
+        return len(text_file)
+    else:
+        return len(content)
     
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Process some command-line arguments.")
-    parser.add_argument("-l", "--lines", action="store_true", default=False, help="lines flag")
-    parser.add_argument("filepath", type=str, help="The first argument")
+    parser.add_argument("-l", "--lines", action="store_true", help="lines flag")
+    parser.add_argument("-w", "--words", action="store_true", help="word flag")
+    parser.add_argument("-c", "--characters", action="store_true", help="character flag")
+    parser.add_argument("-f", "--filepath", type=str, required=False, help="The first argument")
+   
     args = parser.parse_args()
+    if args.filepath:
+        inputs = { "filepath": Path(args.filepath) }
+    else:
+        inputs = { "content": sys.stdin.read() }
+
+    display_num: str
+    display_file: str
+    if args.filepath:
+        display_file = args.filepath
+    else:
+        display_file = ""
 
     if args.lines:
-        print(line_count(args.filepath), args.filepath)
+        display_num = str(line_count(**inputs))
+    elif args.words:
+        display_num = str(word_count(**inputs))
+    elif args.characters:
+        display_num = str(character_count(**inputs))
     else:
-        print(byte_count(args.filepath), args.filepath)
+        display_num = f"{line_count(**inputs)} {word_count(**inputs)} {byte_count(**inputs)}"
+
+    print(display_num, display_file)
     
--- tests/test_wc.py
+++ tests/test_wc.py
@@ -1,16 +1,19 @@
 import pytest
 from pathlib import Path
-from src.wc import byte_count, line_count, word_count
+from src.wc import byte_count, line_count, word_count, character_count
 
 @pytest.fixture
 def file_path():
     return Path(__file__).resolve().parent / "test.txt"
 
 def test_byte_count(file_path):
-    assert byte_count(file_path) == 342190
+    assert byte_count(file_path) == 335042
 
 def test_line_count(file_path):
     assert line_count(file_path) == 7145
 
 def test_word_count(file_path):
     assert word_count(file_path) == 58164
+
+def test_character_count(file_path):
+    assert character_count(file_path) == 332146
